a8b834dde1475adae3637dd2fb15617555780c24
Muhammad Al lail Qadrillah <mallailqadrillah43@gmail.com>
2023-11-17 10:43:17 +0700

    done

--- Raspberry/app.py
+++ Raspberry/app.py
@@ -1,7 +1,29 @@
-from tflite_runtime.interpreter import Interpreter
+from tensorflow.lite.python.interpreter import Interpreter
+import RPi.GPIO as GPIO
 import numpy as np
+import time
 import cv2
 
+class Servo:
+    def __init__(self, servo_pin:int, pwm_period:int=50):
+        self.servo_pin = servo_pin
+        self.pwm_period= pwm_period
+    
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.servo_pin, GPIO.OUT)
+        GPIO.setwarnings(False)
+
+        self.pwm = GPIO.PWM(self.servo_pin, self.pwm_period)
+        self.pwm.start(7)
+
+    def rotate_180(self):
+        "rotate servo to 180 degrees"
+        self.pwm.ChangeDutyCycle(2.0)
+        time.sleep(0.5)
+        self.pwm.ChangeDutyCycle(12.0)
+        time.sleep(0.5)
+
+        GPIO.cleanup()
 class ObjectDetection():
     def __init__(self, model_path:str, webcam_path:str, label_path:str):
         self.model_path = model_path
@@ -30,53 +52,53 @@ class ObjectDetection():
         # Open video capture
         cap = cv2.VideoCapture(self.webcam_path)
 
-        while True:
-            # Read frame from video capture
-            ret, frame = cap.read()
-            if not ret:
-                break
+while True:
+    # Read frame from video capture
+    ret, frame = cap.read()
+    if not ret:
+        break
 
-            # Resize frame to expected shape [1xHxWx3]
-            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            imH, imW, _ = frame.shape
-            image_resized = cv2.resize(image_rgb, (width, height))
-            input_data = np.expand_dims(image_resized, axis=0)
+    # Resize frame to expected shape [1xHxWx3]
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    imH, imW, _ = frame.shape
+    image_resized = cv2.resize(image_rgb, (width, height))
+    input_data = np.expand_dims(image_resized, axis=0)
 
-            # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-            if float_input:
-                input_data = (np.float32(input_data) - input_mean) / input_std
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+    if float_input:
+        input_data = (np.float32(input_data) - input_mean) / input_std
 
-            # Perform the actual detection by running the model with the image as input
-            interpreter.set_tensor(input_details['index'], input_data)
-            interpreter.invoke()
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details['index'], input_data)
+    interpreter.invoke()
 
-            # Retrieve detection results
-            boxes = interpreter.get_tensor(output_details[1]['index'])[0]
-            classes = interpreter.get_tensor(output_details[3]['index'])[0]
-            scores = interpreter.get_tensor(output_details[0]['index'])[0]
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[1]['index'])[0]
+    classes = interpreter.get_tensor(output_details[3]['index'])[0]
+    scores = interpreter.get_tensor(output_details[0]['index'])[0]
 
-            detections = []
+    detections = []
 
 
-            # Loop over all detections and draw detection box if confidence is above minimum threshold
-            for i in range(len(scores)):
-                if ((scores[i] > min_conf) and (scores[i] <= 1.0)):
-                    # Get bounding box coordinates and draw box
-                    ymin = int(max(1,(boxes[i][0] * imH)))
-                    xmin = int(max(1,(boxes[i][1] * imW)))
-                    ymax = int(min(imH,(boxes[i][2] * imH)))
-                    xmax = int(min(imW,(boxes[i][3] * imW)))
-                    cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+# Loop over all detections and draw detection box if confidence is above minimum threshold
+for i in range(len(scores)):
+    if ((scores[i] > min_conf) and (scores[i] <= 1.0)):
+        # Get bounding box coordinates and draw box
+        ymin = int(max(1,(boxes[i][0] * imH)))
+        xmin = int(max(1,(boxes[i][1] * imW)))
+        ymax = int(min(imH,(boxes[i][2] * imH)))
+        xmax = int(min(imW,(boxes[i][3] * imW)))
+        cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
-                    # Draw label
-                    object_name = labels[int(classes[i])]
-                    label = '%s: %d%%' % (object_name, int(scores[i]*100))
-                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-                    label_ymin = max(ymin, labelSize[1] + 10)
-                    cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
-                    cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+        # Draw label
+        object_name = labels[int(classes[i])]
+        label = '%s: %d%%' % (object_name, int(scores[i]*100))
+        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+        label_ymin = max(ymin, labelSize[1] + 10)
+        cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+        cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
 
-                    detections.append(object_name.upper())
+        detections.append(object_name.upper())
             
             # IF ORGANIK ROTATE SERVO
             organik = ['AMPAS TEBU', 'RANTING KAYU', 'DAUN', 'KULIT TELUR']
@@ -101,7 +123,7 @@ class ObjectDetection():
 
 if __name__ == '__main__':
   # Set up variables for running user's model
-  PATH_TO_VIDEO = './video-test.mp4'      # Path to Video Capture 
+  PATH_TO_VIDEO = './video-test.mp4' #0 #'./video-test.mp4'      # Path to Video Capture 
   PATH_TO_MODEL = './model/detect.tflite' # Path to model tflite
   PATH_TO_LABELS = './model/labelmap.txt' # Path to labelmap.txt file
 
